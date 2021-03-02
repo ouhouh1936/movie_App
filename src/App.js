c72c83fdf845b12b568067cb2e491c096488a09d
ouhouh1936 <ouhouh1936@gamil.com>
2021-03-02 15:07:28 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="1" />
+      <Food fav="2imchi" />
+      <Food fav="3 imchi" />
     </div>
   );
 }
